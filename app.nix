1cbd4a5de135a4d6bf7d3bd2304b62b9a65ff1f0
IvanPostu <ipostu20000127@gmail.com>
2026-02-20 15:57:09 +0200

    feat: implement full lifecycle of consumer driven contract testing

--- app.nix
+++ app.nix
@@ -7,6 +7,8 @@ pkgs.mkShell {
 
   buildInputs = [
     pkgs.nodejs_24
+    # required by ruby -> @pact-foundation cli tool to upload contracts
+    pkgs.libxcrypt-legacy
   ];
 
   LANG = "en_US.UTF-8";
@@ -14,6 +16,7 @@ pkgs.mkShell {
 
   shellHook = ''
     export PROJECT_ROOT=${PROJECT_ROOT}
+    export LD_LIBRARY_PATH="${pkgs.libxcrypt-legacy}/lib:$LD_LIBRARY_PATH"
 
     export JAVA_HOME=${pkgs.jdk21}
     export PATH=${pkgs.jdk21}/bin:$PATH
